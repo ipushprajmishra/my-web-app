70bcc9febc4ef91ba47d76519ee15770b9cdf85a
Pushpraj Mishra <pushparajsonu1@gmail.com>
2026-02-15 17:50:52 +0530

    added some string to check if old build can be deployed.

--- my-web-app/Controllers/WeatherForecastController.cs
+++ my-web-app/Controllers/WeatherForecastController.cs
@@ -39,7 +39,7 @@ namespace my_web_app.Controllers
         [HttpGet]
         public string GetName(string name)
         {
-            return "your name is " + name;
+            return "your name is old code" + name;
         }
 
         // ✅ NEW: CI/CD version visibility endpoint
