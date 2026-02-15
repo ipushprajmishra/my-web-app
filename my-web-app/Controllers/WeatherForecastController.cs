54474ff3c9e41eb77edd9d04c128accf050c6037
Pushpraj Mishra <pushparajsonu1@gmail.com>
2026-02-15 16:45:19 +0530

    Add project files.

---
+++ my-web-app/Controllers/WeatherForecastController.cs
@@ -0,0 +1,37 @@
+using Amazon.SecretsManager;
+using Microsoft.AspNetCore.Mvc;
+
+namespace my_web_app.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+
+            Console.WriteLine(DateTime.UtcNow + "this is pushprah code");
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
